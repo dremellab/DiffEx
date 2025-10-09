26508b270b09adc4344443f9f5bbbcac24662d10
seqinfomics <cud2td@virginia.edu>
2025-10-09 10:01:21 -0400

    chore: update build_docker script

--- build_docker.sh
+++ build_docker.sh
@@ -3,5 +3,5 @@
 commit_id=$1
 docker buildx build --platform linux/amd64 \
  --build-arg DIFFEX_COMMIT=${commit_id} \
- -t seqinfomics/diffex:0.5.0-dev -f Dockerfile .  --no-cache > build.log 2>&1 &
+ -t seqinfomics/diffex:0.5.1 -f Dockerfile .  --no-cache > build.log 2>&1 &
 echo "Run: tail -f build.log"
